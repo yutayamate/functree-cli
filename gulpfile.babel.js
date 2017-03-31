fa8e4396666ade9f35e7ca4fec2fb2585e4eca75
Yuta Yamate <yyamate@bio.titech.ac.jp>
2017-04-01 03:42:56 +0900

    Configure ESLint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,17 @@
-'use strict'
+'use strict';
 
 import gulp from 'gulp'
+import eslint from 'gulp-eslint'
 import babel from 'gulp-babel'
 import plumber from 'gulp-plumber'
 
+gulp.task('lint', () => {
+    return gulp.src('./src/*.js')
+        .pipe(plumber())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
 
 gulp.task('babel', () => {
     return gulp.src('./src/*.js')
